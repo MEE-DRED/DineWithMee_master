3f625c450d3849c63c2632e832947f9cbc273900
unknown <olawaleprofjobs15@gmail.com>
2026-06-01 20:36:26 -0700

    conssultation component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,14 @@
-// import React, { useEffect } from 'react';
-// // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './components/Home.jsx';
-// import Navbar from './Navbar.jsx';
 
-
-// function App() {
-  
-
-//   return (
-  
-//         <Router>
-//           <div className="min-h-screen flex flex-col bg-gray-900 text-white">
-//             <Navbar />
-//             <main className="flex-1 pt-20">
-//              <Routes>
-//               {/* <Route path="/" element={<Home />} /> */}
-//             </Routes>
-//             </main>
-            
-//           </div>
-//         </Router>
-    
-//   );
-// }
-
-// export default App;
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import NutritionDashboard from "./components/NutritionDashboard.jsx";
-// import MealPlan from "./components/MealPlan.jsx";
-// import Healthprofile from "./components/Healthprofile.jsx";
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="min-h-screen bg-transparent text-gray-900">
-//         <Routes>
-//           {/* Default Route falls back to dashboard */}
-//           <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          
-//           {/* Core App Module Routes */}
-//           <Route path="/dashboard" element={<NutritionDashboard />} />
-//           <Route path="/meals" element={<MealPlan />} />
-//           <Route path="/healthprofile" element={<Healthprofile />} />
-          
-//           {/* Fallback Catch-all */}
-//           <Route path="*" element={<Navigate to="/dashboard" replace />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import NutritionDashboard from "./component/NutritionDashboard.jsx";
-import MealPlan from "./component/MealPlan.jsx";
-import Healthprofile from "./component/Healthprofile.jsx";
-import Consultations from "./component/Consultations.jsx";
-import Orders from "./component/Orders.jsx";
-import SecuritySettings from "./component/SecuritySettings.jsx";
-import Subscriptions from "./component/Subscriptions.jsx";
+import NutritionDashboard from "./components/NutritionDashboard.jsx";
+import MealPlan from "./components/MealPlan.jsx";
+import Healthprofile from "./components/Healthprofile.jsx";
+import Consultations from "./components/Consultations.jsx";
+import Orders from "./components/Orders.jsx";
+import SecuritySettings from "./components/SecuritySettings.jsx";
+import Subscriptions from "./components/Subscriptions.jsx";
 
 function App() {
   return (
